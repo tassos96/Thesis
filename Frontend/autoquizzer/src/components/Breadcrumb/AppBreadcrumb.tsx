941fcca8626eaecd5c20ccc464cdeca7140b56ba
tassos96 <tassos.antono96@gmail.com>
2023-07-09 17:22:39 +0300

    Make breadcrumb dynamic

--- Frontend/autoquizzer/src/components/Breadcrumb/AppBreadcrumb.tsx
+++ Frontend/autoquizzer/src/components/Breadcrumb/AppBreadcrumb.tsx
@@ -1,23 +1,45 @@
 import { Breadcrumb } from "antd";
+import { Link, Route, useLocation } from "react-router-dom";
+import { AppRoutes } from "../../helpers/AppConstants";
+import Home from "../../pages/Home/Home";
 
 export interface IAppBreadcrumbProps {
   path: string;
 }
 
-const AppBreadcrumb = (props: IAppBreadcrumbProps) => {
+const breadcrumbNameMap: Record<string, string> = {
+  '/login': 'Σύνδεση',
+  '/signup': 'Εγγραφή',
+  '/logout': 'Αποσύνδεση',
+  '/account-info': 'Πληροφορίες Λογαριασμού',
+};
 
-  const breadcrumbItems = props.path.split("/");
-  const breadcrumb = breadcrumbItems.map((item, index) => {
-    return <Breadcrumb.Item key={"breabcrump_key_"+index}>{item}</Breadcrumb.Item>;
+const AppBreadcrumb = (props: IAppBreadcrumbProps) => {
+  const location = useLocation();
+  const pathSnippets = location.pathname.split('/').filter((i) => i);
+
+  const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+    return {
+      key: url,
+      title: <Link to={url}>{breadcrumbNameMap[url]}</Link>,
+    };
   });
-  
+
+const breadcrumbItems = [
+  {
+    
+    title: <Link to="/">Αρχική</Link>,
+    key: 'home',
+  },
+].concat(extraBreadcrumbItems);
+
   return (
     <>
-      {breadcrumb?.length && (
-        <Breadcrumb style={{ margin: "16px 0" }}>{breadcrumb}</Breadcrumb>
-      )}
+      <Breadcrumb items={breadcrumbItems} />
     </>
   );
 };
+
 
 export default AppBreadcrumb;
